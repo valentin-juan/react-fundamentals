2dc96d9b0e52d35642fa372415ee97adef1e4e36
Juan A. Valentin <valentinj@Juans-Mac-Pro.local>
2024-02-06 06:16:33 -0300

    Structure application - Sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import { Post } from './Post';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
+
+import styles from './App.module.css';
 
 import './global.css';
 
 export function App() {
   return (
     <>
-    <Header />
-      <Post 
-        author="Juan Valentin"
-        content="Test"/>
-      <Post 
-        author="Rafaele Valentin"
-        content="Um novo post muito legal"/>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post 
+            author="Juan Valentin"
+            content="Test"/>
+          <Post 
+            author="Rafaele Valentin"
+            content="Um novo post muito legal"/>
+        </main>
+      </div>
     </>
   )
 }
