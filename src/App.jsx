703c386b40225cf9599acf421f51eba7b8eeb8da
Juan A. Valentin <valentinj@Juans-Mac-Pro.local>
2024-02-06 06:48:36 -0300

    Structure application - Post component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { Post } from './Post';
 import { Header } from './components/Header';
 import { Sidebar } from './components/Sidebar';
+import { Post } from './components/Post';
 
 import styles from './App.module.css';
 
---
+++ src/components/Post.jsx
@@ -0,0 +1,25 @@
+import styles from './Post.module.css';
+
+export function Post() {
+   return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/valentin-juan.png" />
+                    <div className={styles.authorInfo}>
+                        <strong>Juan Valentin</strong>
+                        <span>Fullstack Developer</span>
+                    </div>
+                </div>
+                <time title='02 de Fev. às 06:42' dateTime='2023-02-06 06:42:00'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+        </article>
+   ) 
+}
